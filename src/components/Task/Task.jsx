88d883358e47033d614e6c5b7868293ee398d0b3
Bvaishnavi123 <97448974+Bvaishnavi123@users.noreply.github.com>
2022-06-11 01:56:00 +0530

    updated

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import styles from "./task.module.css";
 import {Counter} from '../Counter/index'
 import data from '../../data/tasks.json'
+import {useState} from 'react'
 
 const Task = () => {
   // NOTE: do not delete `data-testid` key value pair
+  const[done,setDone] = useState(true)
   return (
     <>
     {data.map((e)=>{
       return <li data-testid="task" className={styles.task}>
-      <input type="checkbox" data-testid="task-checkbox" /> 
-      <div data-testid="task-text">{e.text}</div>
+      <input type="checkbox" data-testid="task-checkbox" onChange={(e)=>{
+      
+      }} /> 
+      <div data-testid="task-text" className={done!==e.done?styles.underline:styles.underlineremove}>{e.text}</div>
      <Counter count={e.count}/>
       <button data-testid="task-remove-button">Remove</button>
     </li>
